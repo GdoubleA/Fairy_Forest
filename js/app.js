e3a4af21b6d0d5838affa27d52829e36633d9d36
GdoubleA <supertemac@gmail.com>
2023-01-20 22:49:18 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
